ac600831fe23bc5f04bb0467bb8a0ad19ae43314
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-07-14 16:31:38 +0000

    adding ex3

--- ch2.go
+++ ch2.go
@@ -7,6 +7,7 @@ import (
 func main(){
   ex1()
   ex2()
+  ex3()
 }
 
 func ex1(){
@@ -21,4 +22,15 @@ func ex2(){
   var i int = value
   var f float64 = value
   fmt.Println(i, f)
+}
+
+func ex3(){
+  var b byte = 255
+  var smallI int32 = 2147483647
+  var bigI uint64 = 18446744073709551615
+
+  b = b + 1
+  smallI = smallI + 1
+  bigI = bigI + 1
+  fmt.Println(b, smallI, bigI)
 }
