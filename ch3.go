ffd9843d6e6a8c2c41de4082c918d2560c45eed1
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-07-14 17:00:17 +0000

    ex3 len and cap and growing

--- ch3.go
+++ ch3.go
@@ -2,12 +2,34 @@ package main
 
 import "fmt"
 
-func main(){
-  nilSlice()
+func main() {
+	nilSlice()
+  ex3_1()
 }
 
-func nilSlice(){
-  var test[] int
+func nilSlice() {
+	var test []int
 
-  fmt.Println(test == nil)
+	fmt.Println(test == nil)
+}
+
+func ex3_1() {
+	var x []int
+	fmt.Println(x, len(x), cap(x))
+
+	x = append(x, 10)
+	fmt.Println(x, len(x), cap(x))
+
+	x = append(x, 20)
+	fmt.Println(x, len(x), cap(x))
+
+	x = append(x, 30)
+	fmt.Println(x, len(x), cap(x))
+
+	x = append(x, 40)
+	fmt.Println(x, len(x), cap(x))
+
+	x = append(x, 50)
+	fmt.Println(x, len(x), cap(x))
+
 }
