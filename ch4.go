6a2dca9aa57d63e1d75c999694f1d35d6eb44ace
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-07-24 20:03:37 +0000

    switch statements

--- ch4.go
+++ ch4.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	ex4_13()
+	ex4_21()
 }
 
 func ex4_1() {
@@ -65,4 +65,48 @@ func ex4_13() {
 	for i, v := range evenVals {
 		fmt.Println(i, v)
 	}
+}
+
+func ex4_15() {
+	m := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+	for i := 0; i < 3; i++ {
+		fmt.Println("Loop", i)
+		for k, v := range m {
+			fmt.Println(k, v)
+		}
+	}
+}
+
+func ex4_19() {
+	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+	for _, word := range words {
+		switch size := len(word); size {
+		case 1, 2, 3, 4:
+			fmt.Println(word, "is a short word!")
+		case 5:
+			wordLen := len(word)
+			fmt.Println(word, "Is the exact right length", wordLen)
+		case 6, 7, 8, 9:
+		default:
+			fmt.Println(word, "is a long word")
+		}
+	}
+}
+
+func ex4_21() {
+	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+  for _,word := range words {
+    switch wordLen := len(word); {
+    case wordLen < 5:
+      fmt.Println(word, "is a short word")
+    case wordLen > 10:
+      fmt.Println(word, "is a long word")
+    default:
+      fmt.Println(word, "Is the perfect length!")
+    }
+  }
 }
