353a44d9f37dcd59540f4adb400f45f3e8b547f6
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-07-24 19:27:28 +0000

    if scope declaration

--- ch4.go
+++ ch4.go
@@ -6,26 +6,25 @@ import (
 )
 
 func main() {
-  ex4_5()
+	ex4_5()
 }
 
-func ex4_1(){
-  x := 10
-  if x > 5 {
-    fmt.Println(x)
-    x := 5
-    fmt.Println(x)
-  }
-  fmt.Println(x)
+func ex4_1() {
+	x := 10
+	if x > 5 {
+		fmt.Println(x)
+		x := 5
+		fmt.Println(x)
+	}
+	fmt.Println(x)
 }
 
-func ex4_5(){
-  n := rand.Intn(10)
-  if n == 0 {
-    fmt.Println("Too Low") 
-  } else if n > 5 {
-    fmt.Println("That's too big") 
-  } else {
-    fmt.Println("That's perfect")
-  }
+func ex4_5() {
+	if n := rand.Intn(10);n == 0 {
+		fmt.Println("Too Low")
+	} else if n > 5 {
+		fmt.Println("That's too big")
+	} else {
+		fmt.Println("That's perfect")
+	}
 }
