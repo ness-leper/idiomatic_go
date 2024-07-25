60c3e1a49d896f8bd66c2926425c9123ffcbf27d
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-07-25 02:32:37 +0000

    exercise 2

--- ch4.go
+++ ch4.go
@@ -6,15 +6,35 @@ import (
 )
 
 func main() {
-	exercise1()
+	exercise2()
 }
 
 func exercise1() {
 	stored := make([]int, 0, 100)
 	for i := 0; i < 100; i++ {
-    stored = append(stored, rand.Intn(100))
+		stored = append(stored, rand.Intn(100))
 	}
 	fmt.Println(stored)
+}
+
+func exercise2() {
+	stored := make([]int, 0, 100)
+	for i := 0; i < 100; i++ {
+		stored = append(stored, rand.Intn(100))
+	}
+
+	for _, v := range stored {
+		switch {
+		case v%6 == 0:
+			fmt.Println("six")
+		case v%3 == 0:
+			fmt.Println("three")
+		case v%2 == 0:
+			fmt.Println("two")
+		default:
+			fmt.Println("never mind")
+		}
+	}
 }
 
 func ex4_1() {
