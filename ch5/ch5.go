05405480d34459c09b72b8483b15694215746a4a
Nicholas <nmonforte@gmail.com>
2024-07-31 21:57:57 -0400

    multiple return values

--- ch5/ch5.go
+++ ch5/ch5.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main()  {
   fmt.Println(addToBase(3))
   fmt.Println(addToBase(3,2))
+
+  result, remainder, err := divAndRemainder(5,2)
+
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  fmt.Println(result, remainder)
 }
 
 func addToBase(base int, vals ...int) []int {
@@ -13,4 +25,12 @@ func addToBase(base int, vals ...int) []int {
     out = append(out, base + v)
   }
   return out
+}
+
+func divAndRemainder(num, denom int) (int, int, error) {
+  if denom == 0 {
+    return 0, 0, errors.New("cannot divide by zero")
+  }
+
+  return num / denom, num % denom, nil
 }
