8752c71feaccb0d6683c9fb23259801624fafb3d
Nicholas <nmonforte@gmail.com>
2024-07-31 21:53:19 -0400

    variadic parameteres

--- ch5/ch5.go
+++ ch5/ch5.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main()  {
- fmt.Println("Hello World") 
+  fmt.Println(addToBase(3))
+  fmt.Println(addToBase(3,2))
+}
+
+func addToBase(base int, vals ...int) []int {
+  out := make([]int,0,len(vals))
+  for _, v := range vals {
+    out = append(out, base + v)
+  }
+  return out
 }
