52bf4aad76854666976a0dd3cfda4213d347686b
Nicholas <nmonforte@gmail.com>
2024-08-05 20:15:18 -0400

    methods on a struct

--- ch7/ch7.go
+++ ch7/ch7.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello Chapter 7!")
+	fmt.Println("Hello Chapter 7!")
+	p := Person{
+		FirstName: "John",
+		LastName:  "Smith",
+		Age:       25,
+	}
+
+  fmt.Println(p.String())
+}
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%s %s, age %d", p.FirstName, p.LastName, p.Age)
 }
