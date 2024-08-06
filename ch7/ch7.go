ef4910127dd7bedafdf3efbcf9bfb7b07fd10f07
Nicholas <nmonforte@gmail.com>
2024-08-05 20:29:38 -0400

    pointers and not

--- ch7/ch7.go
+++ ch7/ch7.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello Chapter 7!")
@@ -10,7 +13,15 @@ func main() {
 		Age:       25,
 	}
 
-  fmt.Println(p.String())
+	fmt.Println(p.String())
+
+	var c Counter
+	fmt.Println(c.String())
+	c.Increment()
+	fmt.Println(c.String())
+
+  doUpateWrong(c)
+  // doUpdateRight(&c)
 }
 
 type Person struct {
@@ -21,4 +32,28 @@ type Person struct {
 
 func (p Person) String() string {
 	return fmt.Sprintf("%s %s, age %d", p.FirstName, p.LastName, p.Age)
+}
+
+type Counter struct {
+	total       int
+	lastUpdated time.Time
+}
+
+func (c *Counter) Increment() {
+	c.total++
+	c.lastUpdated = time.Now()
+}
+
+func (c Counter) String() string {
+	return fmt.Sprintf("total: %d, last updated: %v", c.total, c.lastUpdated)
+}
+
+func doUpateWrong(c Counter) {
+	c.Increment()
+	fmt.Println("in doUpdateWrong:", c.String())
+}
+
+func doUpdateRight(c *Counter) {
+  c.Increment()
+  fmt.Println("in doUpdateRight:", c.String())
 }
